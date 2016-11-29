b52fbd3dcc8b9920376f9a00b47fd899db9d3698
Clinton <clintoncruise@gmail.com>
2016-11-29 12:37:48 +0800

    TableView and CollectionView
    Code to generate cells complete

--- Kerrent/FrontPageViewController.swift
+++ Kerrent/FrontPageViewController.swift
@@ -31,7 +31,7 @@ class FrontPageViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 4
+        return model.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -40,11 +40,27 @@ class FrontPageViewController: UIViewController, UITableViewDelegate, UITableVie
         return cell
     }
     
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let tableViewCell = cell as? FrontpageTableViewCell else {return}
+        
+        tableViewCell.collectionViewDidLoad(dataSourceDelegate: self, forRow: indexPath.row)
+    }
 
 }
 
-extension UICollectionViewDelegate {
+extension FrontPageViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return model[collectionView.tag].count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        cell.backgroundColor = model.[collectionView.tag].[indexPath.item]
+        
+        return cell
+    }
 }
 
 
