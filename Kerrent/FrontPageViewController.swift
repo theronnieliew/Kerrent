613c46fe5006bfaf34a0547c3911f3a3bb170ac5
Clinton <clintoncruise@gmail.com>
2016-11-29 12:18:34 +0800

    TableViewCell, GenerateColors Helper
    Configured tableViewCell to hold a collectionView
    
    Added a generateColors helper from
    https://github.com/ashfurrow/Collection-View-in-a-Table-View-Cell/blob/m
    aster/Table%20View%20in%20a%20Collection%20View/Helpers.swift

--- Kerrent/FrontPageViewController.swift
+++ Kerrent/FrontPageViewController.swift
@@ -15,19 +15,12 @@ class FrontPageViewController: UIViewController, UITableViewDelegate, UITableVie
 
     @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var collectionView: UICollectionView!
+    let model : [[UIColor]] = generateRandomData()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-
+        tableDidLoad()
     }
     
     func tableDidLoad() {
@@ -47,16 +40,11 @@ class FrontPageViewController: UIViewController, UITableViewDelegate, UITableVie
         return cell
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension UICollectionViewDelegate {
+    
+}
+
+
