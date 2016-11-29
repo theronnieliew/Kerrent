8290cf9f58f79d8474ec44c78e7892c84b30dcb5
Clinton <clintoncruise@gmail.com>
2016-11-29 09:50:20 +0800

    Storyboard & ViewController
    Added custom storyboard and first view controller

---
+++ Kerrent/FrontPageViewController.swift
@@ -0,0 +1,62 @@
+//
+//  FrontPageViewController.swift
+//  Kerrent
+//
+//  Created by Clinton Otten on 29/11/2016.
+//  Copyright © 2016 Panda^4. All rights reserved.
+//
+
+import UIKit
+
+class FrontPageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+//Variables
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+
+    }
+    
+    func tableDidLoad() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
