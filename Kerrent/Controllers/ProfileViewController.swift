524c05224cda7559fa56335faedf33b275ac1bcf
Ronnie <theronnieliew@gmail.com>
2016-11-30 15:11:15 +0800

    Push base profile view controller and storyboard

---
+++ Kerrent/Controllers/ProfileViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var profilePicImgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
