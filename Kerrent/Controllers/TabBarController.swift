d6062a905d05bc22cb2bc87fef16cc4c7d47ab6b
Ronnie <theronnieliew@gmail.com>
2016-12-02 18:06:11 +0800

    Added tab bar shit

---
+++ Kerrent/Controllers/TabBarController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        loadViews(storyboard: "Frontpage", controllername: "FrontPageViewController")
+    }
+//
+//    func loadViews(storyboard : String, controllername : String){
+//        let storyboard = UIStoryboard(name: storyboard, bundle: nil)
+//        let controller = storyboard.instantiateViewController(withIdentifier: controllername)
+//        addChildViewController(controller)
+//        view.addSubview(controller.view)
+//        controller.didMove(toParentViewController: self)
+//    }
+}
