f3e3aa405d81ecd08efd41356c36323ed22b20a3
Clinton <clintoncruise@gmail.com>
2016-12-06 15:23:18 +0800

    New Class for Firebase Fetch
    To populate tableview

--- Kerrent/InstaCarPost.swift
+++ Kerrent/InstaCarPost.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+class Rent{
+    var car = ""
+    var dateEnd = ""
+    var dateStart = ""
+    var location = ""
+    var owner = ""
+    var ownerID = ""
+    var price = ""
+}
