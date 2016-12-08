168c9c2a01310949eec6e61ec92dd6c27f1973d4
Clinton <clintoncruise@gmail.com>
2016-12-08 12:44:24 +0800

    Added RentFeed
    Added Storyboard, TableViewCell, ViewController.
    
    Alternative to current FrontPage stuff.

---
+++ Kerrent/RentFeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RentFeedTableViewCell.swift
+//  Kerrent
+//
+//  Created by Clinton Otten on 08/12/2016.
+//  Copyright © 2016 Panda^4. All rights reserved.
+//
+
+import UIKit
+
+class RentFeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
