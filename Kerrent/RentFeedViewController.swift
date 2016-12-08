5f3278e6944a4e95df6cedd5c66e7bacebb37df3
Clinton <clintoncruise@gmail.com>
2016-12-08 19:02:46 +0800

    RentFeed Code

--- Kerrent/RentFeedViewController.swift
+++ Kerrent/RentFeedViewController.swift
@@ -8,20 +8,35 @@
 
 import UIKit
 
-class RentFeedViewController: UIViewController {
+class RentFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    // VARIABLES
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableDidLoad() {
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+        
+    }
     /*
     // MARK: - Navigation
 
